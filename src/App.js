79c4afb59ef039571141785792e8d9634d224a97
saanyalall <91753206+saanyalall@users.noreply.github.com>
2023-06-21 22:07:14 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Home from './components/Home';
-// import style from './components/style';
+
 
 
 function App() {
   return (
     <div>
     <Home/>
-    {/* <style/> */}
+    
     
     </div>
   );
